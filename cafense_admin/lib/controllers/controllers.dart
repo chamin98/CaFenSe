160698fc36a532da4cb7e5f47ddf049aa6c51683
Star4ze <72427067+Star4ze@users.noreply.github.com>
2021-10-25 21:36:21 +0530

    Delete cafense_admin/lib directory

--- cafense_admin/lib/controllers/controllers.dart
+++
@@ -1,6 +0,0 @@
-import 'package:cafense_admin/controllers/menu_controller.dart';
-import 'package:cafense_admin/controllers/navigation_controller.dart';
-
-MenuController menuController = MenuController.instance;
-NavigationController navigationController = NavigationController.instance;
-
